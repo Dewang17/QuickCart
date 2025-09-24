21251bb29eb1cc6ac7440663a743333b4445e75a
dewang17 <dewangsankadiya9@gmail.com>
2025-09-24 17:39:26 +0530

    update

--- config/db.js
+++ config/db.js
@@ -13,7 +13,7 @@ async function connectDB() {
       bufferCommands: false,
     };
     cached.promise = (
-      await mongoose.connect(`${process.env.MONGO_URI}/quickcart`, opts)
+     await  mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts)
     ).then((mongoose) => {
       return mongoose;
     });
